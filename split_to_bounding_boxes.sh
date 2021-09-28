b0c3aa2936b9596b9e498eb2705400995096a11e
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2021-09-28 15:18:32 +0100

    Add bash script to slice data in squares

---
+++ split_to_bounding_boxes.sh
@@ -0,0 +1,41 @@
+datafile=$1
+ratio=$2
+# bbox=$(osmium fileinfo -eg data.bbox $datafile | sed -e 's/[()]//g')
+bbox=$(osmium fileinfo $datafile | grep ' \+(-\?[0-9]\+.[0-9]\+,')
+
+xmin0=$(echo $bbox | sed -e 's/[()]//g' | cut -d, -f1)
+ymin0=$(echo $bbox | sed -e 's/[()]//g' | cut -d, -f2)
+xmax0=$(echo $bbox | sed -e 's/[()]//g' | cut -d, -f3)
+ymax0=$(echo $bbox | sed -e 's/[()]//g' | cut -d, -f4)
+
+echo "#################"
+echo xmin = $xmin0
+echo ymin = $ymin0
+echo xmax = $xmax0
+echo ymax = $ymax0
+echo "#################"
+echo
+echo
+	
+dlong=$(echo "($xmax0 - $xmin0) / $ratio" | bc -l)
+dlat=$(echo "($ymax0 - $ymin0) / $ratio" | bc -l)
+for i in $(seq 1 $ratio); do
+    xmin=$(echo "$xmin0 + ($i - 1) * $dlong" | bc -l)
+    xmax=$(echo "$xmin0 + $i * $dlong" | bc -l)
+    for j in $(seq 1 $ratio); do
+	ymin=$(echo "$ymin0 + ($j - 1)* $dlat" | bc -l)
+	ymax=$(echo "$ymin0 + $j * $dlat" | bc -l)
+
+	slug=${datafile%.osm.pbf}
+	n=$(($j + $ratio*$(($i-1))))
+	outputfile=${slug}_slice${n}.osm.pbf
+	echo "Slicing slice $n"
+	osmium extract --no-progress --bbox $xmin,$ymin,$xmax,$ymax $datafile -o $outputfile
+    done
+done
+	
+	
+	
+	 
+
+
