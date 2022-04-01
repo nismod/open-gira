80f1e4c2da2b6c9616c3e3a2bec9e63a59d06580
Matt Jaquiery <matt.jaquiery@cs.ox.ac.uk>
2022-04-01 20:24:17 +0100

    make_exposure_tif.py
    Streamline make_exposure_img to preprocess the coastline/boundary data once per dataset then apply to all hazard tifs.

--- workflow/scripts/make_exposure_img.py
+++ workflow/scripts/make_exposure_img.py
@@ -10,17 +10,19 @@ import rasterio.plot
 import os
 import matplotlib.pyplot as plt
 import shapely.geometry as shape
+import re
+import glob
 
 
 if __name__ == "__main__":
     logging.basicConfig(format="%(asctime)s %(message)s", level=logging.INFO)
-    # move to settings later
-    epsg = 3857
+
     try:
-        hazard_tif = snakemake.input['tif'][0]
+        hazard_dir = snakemake.input['hazard_dir']
         coastline = snakemake.input['coastline']
         boundaries = snakemake.input['boundaries']
-        output_path = snakemake.output[0]
+        output_dir = snakemake.output[0]
+
         try:
             opts_dict = snakemake.config['exposure_tifs']['plot']
         except KeyError:
@@ -29,16 +31,16 @@ if __name__ == "__main__":
     except NameError:
         # print(sys.argv)
         # (
-        #     hazard_tif,
+        #     hazard_dir,
         #     coastline,
         #     boundaries,
         #     output_path,
         #     opts_dict
         # ) = sys.argv[1:]
-        hazard_tif = '../../results/exposure/tanzania-latest_filter-highway-core/hazard-aqueduct-river/exposure_inunriver_rcp4p5_MIROC-ESM-CHEM_2030_rp00100.tif'
+        hazard_dir = '../../results/exposure/tanzania-latest_filter-highway-core/hazard-aqueduct-river/'
         coastline = '../../results/input/coastlines/ne_10m_ocean/ne_10m_ocean.shp'
         boundaries = '../../results/input/admin-boundaries/ne_50m/ne_50m_admin_0_countries.shp'
-        output_path = '../../results/exposure/tanzania-latest_filter-highway-core/hazard-aqueduct-river/img/exposure_inunriver_rcp4p5_MIROC-ESM-CHEM_2030_rp00100.png'
+        output_dir = '../../results/exposure/tanzania-latest_filter-highway-core/hazard-aqueduct-river/img/'
         opts_dict = {}
 
     # Load up the options from the opts_dict
@@ -49,42 +51,52 @@ if __name__ == "__main__":
     coastline_opts = opt('coastline', {'facecolor': '#87cefa'})
     boundary_opts = opt('boundary', {'edgecolor': '#000000'})
 
-    logging.info(f"Generating image {output_path}")
-    if not os.path.exists(os.path.dirname(output_path)):
-        os.makedirs(os.path.dirname(output_path))
-        logging.info(f"Created directory {os.path.dirname(output_path)}")
+    logging.info(f"Saving images to {output_dir}")
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+        logging.info(f"Created directory {output_dir}")
 
-    with rasterio.open(hazard_tif) as hazard:
-        # To plot everything in the same way and in the same place
-        # we need to make sure everything uses the same
-        # Coordinate Reference System (CRS).
-        # The CRS we'll use as the boss is the hazard raster file CRS.
-        plt_crs = hazard.crs.to_epsg()
-        band = hazard.read()
-        hazard_rect = shape.Polygon([
-            (hazard.bounds.left, hazard.bounds.top),
-            (hazard.bounds.right, hazard.bounds.top),
-            (hazard.bounds.right, hazard.bounds.bottom),
-            (hazard.bounds.left, hazard.bounds.bottom),
-        ])
+    logging.debug("Preparing coastline data.")
+    coast = gp.read_file(coastline)
 
-        # Make axes
-        fig, ax = plt.subplots(dpi=300)
+    logging.debug("Preparing administrative boundary data.")
+    bounds = gp.read_file(boundaries)
 
-        # Plot raster
-        logging.debug("Plotting raster data.")
-        rasterio.plot.show(hazard, ax=ax, zorder=1, **raster_opts)
+    hazard_files = glob.glob(os.path.join(hazard_dir, '*.tif'))
+    logging.info(f"Found {len(hazard_files)} .tif files in {hazard_dir}")
 
-        # Plot coastline
-        logging.debug("Plotting coastline data.")
-        coast = gp.read_file(coastline).to_crs(plt_crs)
-        coast = coast.geometry.clip(hazard_rect)
-        coast.plot(ax=ax, edgecolor='none', zorder=2, **coastline_opts)
+    for hazard_tif in hazard_files:
+        with rasterio.open(hazard_tif) as hazard:
+            # To plot everything in the same way and in the same place
+            # we need to make sure everything uses the same
+            # Coordinate Reference System (CRS).
+            # The CRS we'll use as the boss is the hazard raster file CRS.
+            plt_crs = hazard.crs.to_epsg()
+            band = hazard.read()
+            hazard_rect = shape.Polygon([
+                (hazard.bounds.left, hazard.bounds.top),
+                (hazard.bounds.right, hazard.bounds.top),
+                (hazard.bounds.right, hazard.bounds.bottom),
+                (hazard.bounds.left, hazard.bounds.bottom),
+            ])
 
-        # Plot boundaries
-        logging.debug("Plotting administrative boundary data.")
-        bounds = gp.read_file(boundaries).to_crs(plt_crs)
-        bounds = bounds.geometry.clip(hazard_rect)
-        bounds.plot(ax=ax, facecolor='none', zorder=3, **boundary_opts)
+            # Make axes
+            fig, ax = plt.subplots(dpi=300)
 
-        plt.savefig(output_path)
+            # Plot raster
+            logging.debug("Plotting raster data.")
+            rasterio.plot.show(hazard, ax=ax, zorder=1, **raster_opts)
+
+            # Plot coastline
+            coast_tmp = coast.to_crs(plt_crs)
+            coast_tmp = coast_tmp.geometry.clip(hazard_rect)
+            coast_tmp.plot(ax=ax, edgecolor='none', zorder=2, **coastline_opts)
+
+            # Plot boundaries
+            bounds_tmp = bounds.to_crs(plt_crs)
+            bounds_tmp = bounds_tmp.geometry.clip(hazard_rect)
+            bounds_tmp.plot(ax=ax, facecolor='none', zorder=3, **boundary_opts)
+
+            output_path = os.path.join(output_dir, re.sub('\\.tiff?$', '.png', os.path.basename(hazard_tif)))
+            logging.info(f"Saving {output_path}")
+            plt.savefig(output_path)
