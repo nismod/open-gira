f9c30fe3c7638a1001d154f47c2d2d8ab673b3ea
Fred Thomas <frederick.thomas@ouce.ox.ac.uk>
2022-07-26 12:13:48 +0100

    handle road nodes

--- workflow/scripts/create_road_network.py
+++ workflow/scripts/create_road_network.py
@@ -43,15 +43,26 @@ def create_network(
     logging.info("Splitting multilines")
     network = snkit.network.split_multilinestrings(network)
 
-    if nodes is not None:
-        logging.info("Snapping nodes to edges")
-        network = snkit.network.snap_nodes(network)
+    if nodes is not None and not nodes.empty:
+        # check we have only point nodes
+        assert set(network.nodes.geometry.type.values) == {"Point"}
 
         logging.info("Dropping duplicate geometries")
+        # silence shapely.ops.split ShapelyDeprecationWarning regarding:
+        # shapley.ops.split failure on split by empty geometry collection
+        # this is currently caught by snkit.network.split_edge_at_points,
+        # but won't be for shapely==2.0
+        warnings.filterwarnings(
+            "ignore",
+            message=(
+                ".*GeometryTypeError will derive from ShapelyError "
+                "and not TypeError or ValueError in Shapely 2.0*"
+            )
+        )
         network.nodes = snkit.network.drop_duplicate_geometries(network.nodes)
 
-        logging.info("Splitting edges at nodes")
-        network = snkit.network.split_edges_at_nodes(network)
+        logging.info("Snapping nodes to edges")
+        network = snkit.network.snap_nodes(network)
 
     logging.info("Adding endpoints")
     network = snkit.network.add_endpoints(network)
@@ -63,9 +74,7 @@ def create_network(
     assert set(network.edges.geometry.type.values) == {"LineString"}
 
     logging.info("Renaming nodes and edges")
-    network = snkit.network.add_ids(
-        network, edge_prefix=f"{node_edge_prefix}", node_prefix=f"{node_edge_prefix}"
-    )
+    network = snkit.network.add_ids(network)
 
     logging.info("Creating network topology")
     network = snkit.network.add_topology(network, id_col="id")
