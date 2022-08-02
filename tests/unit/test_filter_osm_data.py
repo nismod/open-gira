7b5678ff3716a440986cbc5a154bcd161a9d9914
Fred Thomas <frederick.thomas@ouce.ox.ac.uk>
2022-08-02 17:15:18 +0100

    change to djibouti

--- tests/unit/test_filter_osm_data.py
+++ tests/unit/test_filter_osm_data.py
@@ -8,5 +8,5 @@ sys.path.insert(0, os.path.dirname(__file__))
 def test_filter_osm_data():
     common.run_test(
         "filter_osm_data",
-        "snakemake results/input/tanzania-mini_filter-road.osm.pbf -j1 --keep-target-files",
+        "snakemake results/input/djibouti-latest_filter-road.osm.pbf -j1 --keep-target-files",
     )
