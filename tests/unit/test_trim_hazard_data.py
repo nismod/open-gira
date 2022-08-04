9ec9ccc85fdf9ab26f99c04d8bedfb8b15f6ba10
Fred Thomas <frederick.thomas@ouce.ox.ac.uk>
2022-08-04 13:54:55 +0100

    change to djibouti
    remove input file as prior rule will fetch it from external_files

--- tests/unit/test_trim_hazard_data.py
+++ tests/unit/test_trim_hazard_data.py
@@ -8,5 +8,5 @@ sys.path.insert(0, os.path.dirname(__file__))
 def test_trim_hazard_data():
     common.run_test(
         "trim_hazard_data",
-        "snakemake results/input/hazard-aqueduct-river/tanzania-mini -j1 --keep-target-files",
+        "snakemake results/input/hazard-aqueduct-river/djibouti-latest -j1 --keep-target-files",
     )
