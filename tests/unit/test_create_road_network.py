72ebd7e6405b942350029a896674547b8bc0afa7
Fred Thomas <frederick.thomas@ouce.ox.ac.uk>
2022-08-04 16:11:19 +0100

    note on testing gadm36_levels.gpkg

--- tests/unit/test_create_road_network.py
+++ tests/unit/test_create_road_network.py
@@ -6,6 +6,12 @@ sys.path.insert(0, os.path.dirname(__file__))
 
 
 def test_create_road_network():
+    """
+    N.B. this test requires country geometry data to annotate nodes and edges
+    the data is stored in the input gadm36_levels.gpkg file. This has been
+    manually subsetted from the global file to Djibouti and the three
+    neighbouring countries: Eritrea, Somalia and Ethiopia.
+    """
     common.run_test(
         "create_road_network",
         (
