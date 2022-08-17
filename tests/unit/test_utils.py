bb6166c8b91e20716678b8f156fb64ae022d1421
Fred Thomas <frederick.thomas@ouce.ox.ac.uk>
2022-08-17 16:28:51 +0100

    test transport.utils.str_to_bool

---
+++ tests/unit/test_utils.py
@@ -0,0 +1,36 @@
+import os
+import sys
+
+import numpy as np
+import pandas as pd
+import pytest
+
+from workflow.scripts.transport.utils import str_to_bool
+
+
+class TestStrToBool:
+    def test_str_mapping(self):
+        truth_table = (
+            ('N', False),
+            ('n', False),
+            ('NO', False),
+            ('no', False),
+            ('FALSE', False),
+            ('false', False),
+            ('F', False),
+            ('f', False),
+            ('', False),
+            # N.B. null values are transformed to empty strings
+            # then mapped to false
+            (None, False),
+            (np.nan, False),
+            ('any other string', True),
+        )
+        strings, bools = zip(*truth_table)
+        expected = pd.Series(bools)
+        assert expected.equals(str_to_bool(pd.Series(strings)))
+
+    def test_incorrect_type(self):
+        for data in [True, 1, 1.414]:
+            with pytest.raises(ValueError):
+                str_to_bool(pd.Series([data])) 
