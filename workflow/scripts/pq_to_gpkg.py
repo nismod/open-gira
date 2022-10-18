98531f44e48d4a085680ed59322eb5a25c00eea7
Fred Thomas <fred.thomas@ouce.ox.ac.uk>
2022-10-18 17:18:08 +0100

    drop dtypes incompatible with gpkg

--- workflow/scripts/pq_to_gpkg.py
+++ workflow/scripts/pq_to_gpkg.py
@@ -6,17 +6,46 @@ import os
 import sys
 
 import geopandas as gpd
+import numpy as np
 
 
 GPKG_EXT = ".gpkg"
 PARQUET_EXT = ".geoparquet"
+GPKG_INCOMPATIBLE_CLASSES = (np.ndarray,)
 
 
-def geoparquet_to_geopackage(parquet_path) -> str:
+def geoparquet_to_geopackage(parquet_path: str) -> str:
+    """
+    Rewrite geoparquet file as geopackage in same location (new file extension).
+
+    N.B. If geopackage file contains dtypes that are incompatible with geopackage
+    implementation in fiona, we will drop these columns and try again.
+
+    Arguments:
+        parquet_path (str): Location of geoparquet file
+
+    Returns:
+        str: Location of new geopackage file
+    """
     gdf = gpd.read_parquet(parquet_path)
     stem, _ = os.path.splitext(parquet_path)
     gpkg_path = stem + GPKG_EXT
-    gdf.to_file(gpkg_path)
+
+    try:
+        gdf.to_file(gpkg_path)
+    except ValueError:
+        # fiona will refuse to serialize some data to geopackage, try dropping
+
+        # hunt for bad dtypes -- and hope first row is representative!
+        dtypes = gdf.iloc[0, :].apply(type).values
+        bad_indicies = []
+        for klass in GPKG_INCOMPATIBLE_CLASSES:
+            for i in range(len(dtypes)):
+                if dtypes[i] == klass:
+                    bad_indicies.append(i)
+
+        logging.warning(f"Dropping {gdf.columns[bad_indicies]} as not compatible with GPKG")
+        gdf.drop(columns=gdf.columns[bad_indicies]).to_file(gpkg_path)
     return gpkg_path
 
 
