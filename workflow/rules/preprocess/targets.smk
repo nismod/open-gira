95d49aae97542de131fad299a399f570d9988d2a
Tom Russell <tom.russell@ouce.ox.ac.uk>
2022-10-27 12:32:24 +0100

    Process targets per-box

--- workflow/rules/preprocess/targets.smk
+++ workflow/rules/preprocess/targets.smk
@@ -8,7 +8,8 @@ rule process_target_box:
         population="{OUTPUT_DIR}/input/ghsl/GHS_POP_E2020_GLOBE_R2022A_54009_1000_V1_0.tif",
         targets="{OUTPUT_DIR}/input/gridfinder/targets.tif",
         gdp="{OUTPUT_DIR}/input/GDP/GDP_per_capita_PPP_1990_2015_v2.nc",
+        global_boxes=rules.world_splitter.output.global_boxes,
     output:
-        targets="{OUTPUT_DIR}/processed/power_targets.geoparquet",
+        targets="{OUTPUT_DIR}/processed/power/{BOX}/targets_{BOX}.parquet",
     script:
         "../../scripts/process/process_power_2_targets.py"
--- workflow/scripts/process/process_power_2_targets.py
+++ workflow/scripts/process/process_power_2_targets.py
@@ -1,9 +1,8 @@
 """This file processes the target data
 """
-import sys
-
-import geopandas as gpd
+import geopandas
 import numpy as np
+import pandas
 import rasterio
 import rasterio.features
 import rasterio.mask
@@ -11,10 +10,9 @@ import xarray as xr
 from pyproj import Geod
 from rasterstats import gen_zonal_stats
 from shapely.geometry import shape
-from tqdm import tqdm
 
 
-def get_target_areas(targets_file):
+def get_target_areas(targets_file, box_geom):
     geod = Geod(ellps="WGS84")
     geoms = []
     areas_km2 = []
@@ -22,16 +20,17 @@ def get_target_areas(targets_file):
     # Targets: Binary raster showing locations predicted to be connected to distribution grid.
     with rasterio.open(targets_file) as dataset:
         # Read the dataset's valid data mask as a ndarray.
-        mask = dataset.dataset_mask()
+        box_dataset, box_transform = rasterio.mask.mask(dataset, [box_geom], crop=True)
+
         # Extract feature shapes and values from the array.
-        for geom, val in rasterio.features.shapes(mask, transform=dataset.transform):
+        for geom, val in rasterio.features.shapes(box_dataset, transform=box_transform):
             if val > 0:
                 feature = shape(geom)
                 geoms.append(feature)
                 area_m2, _ = geod.geometry_area_perimeter(feature)
                 areas_km2.append(area_m2 / 1e6)
 
-    return gpd.GeoDataFrame({
+    return geopandas.GeoDataFrame({
         "area_km2": areas_km2,
         "geometry": geoms
     })
@@ -39,16 +38,13 @@ def get_target_areas(targets_file):
 
 def get_population(targets, population_file):
     stats = gen_zonal_stats(
-        targets.geometry,
+        targets.set_crs("epsg:4326").to_crs("esri:54009").geometry,  # reprojected for raster
         population_file,
         stats=[],
         add_stats={"nansum": np.nansum},  # count NaN as zero for summation
         all_touched=True,  # possible overestimate, but targets grid is narrower than pop
     )
-    populations = [
-        d["nansum"]
-        for d in tqdm(stats, desc=f"Population progress", total=len(targets))
-    ]
+    populations = [d["nansum"] for d in stats]
 
     return populations
 
@@ -56,23 +52,32 @@ def get_population(targets, population_file):
 def get_gdp_pc(targets, gdp_file):
     gdp = xr.open_dataset(gdp_file)
     centroids = targets.geometry.centroid
-    gdp_pc = gdp.sel(x=centroids.x, y=centroids.y)
-    return gdp_pc
+    df = pandas.DataFrame({"x": centroids.x, "y": centroids.y})
+    df["gdp_pc"] = df.apply(
+        lambda row: float(gdp.GDP_per_capita_PPP.sel(
+                longitude=row.x,
+                latitude=row.y,
+                time=2015,
+                method="nearest"
+            ).data),
+        axis=1
+    )
+    return df.gdp_pc
 
 
 if __name__ == '__main__':
-    try:
-        population_file = snakemake.input.population  # type: ignore
-        gdp_file = snakemake.input.gdp  # type: ignore
-        targets_file = snakemake.input.targets  # type: ignore
-        output_file = snakemake.output.targets  # type: ignore
-    except:
-        population_file = sys.argv[1]
-        gdp_file = sys.argv[2]
-        targets_file = sys.argv[3]
-        output_file = sys.argv[4]
+    population_file = snakemake.input.population  # type: ignore
+    gdp_file = snakemake.input.gdp  # type: ignore
+    targets_file = snakemake.input.targets  # type: ignore
+    output_file = snakemake.output.targets  # type: ignore
+    global_boxes_path = snakemake.input.global_boxes  # type: ignore
+    box_id = snakemake.wildcards.BOX  # type: ignore
 
-    targets = get_target_areas(targets_file)
+    boxes = geopandas.read_file(global_boxes_path) \
+        .set_index("box_id")
+    box_geom = boxes.loc[f"box_{box_id}", "geometry"]
+
+    targets = get_target_areas(targets_file, box_geom)
     targets["type"] = "target"
     targets["population"] = get_population(targets, population_file)
     targets["gdp_pc"] = get_gdp_pc(targets, gdp_file)
