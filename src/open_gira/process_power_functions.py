5bfbe092ec8c74eef3d50eac2a650b79a2ca9de6
Tom Russell <tom.russell@ouce.ox.ac.uk>
2022-10-31 14:51:31 +0000

    Fix adjacent box index calculation

--- src/open_gira/process_power_functions.py
+++ src/open_gira/process_power_functions.py
@@ -1,95 +1,46 @@
 """Common functions required to perform preprocessing
 """
 
-def adj(idx, num_cols, tot_boxes):
-    """Returns the indices of all boxes that touch the idx box (incl at corners). Note the top and bottom are cut offs but either left or right side is connected
-    num_cols - number of cols (i.e. number of boxes across the equator
-    tot_boxes - total number of boxes
 
-    Note top left is (-,+), top right is (+,+), bottom right is (+,-) bottom left is (-,-)"""
-    assert tot_boxes % num_cols == 0
+def adj(idx: int, ncols: int, tot_boxes: int) -> list[int]:
+    """Returns the indices of all boxes that touch the idx box (incl at corners).
+
+    Note the assumption that index 0 (or (0,0)) is top-left.
+
+    Parameters
+    ----------
+    ncols: int, number of cols (i.e. number of boxes across the equator
+    tot_boxes: int, total number of boxes
+    """
+    assert tot_boxes % ncols == 0
     assert 0 <= idx <= tot_boxes
-    left, right = False, False
-    adjacent = []
-    if idx % num_cols == 0:  # on left boundary
-        adjacent += [
-            idx + num_cols - 1,
-            idx + 1,
-        ]  # adds [idx on right boundary (same row), one to the right]
-        left = True
-    elif (idx + 1) % num_cols == 0:  # on right boundary
-        adjacent += [
-            idx - num_cols + 1,
-            idx - 1,
-        ]  # adds [idx on left boundary (same row), one to left]
-        right = True
-    else:
-        adjacent += [idx - 1, idx + 1]  # adds [left, right]
+    nrows = tot_boxes / ncols
 
-    if 0 <= idx <= num_cols - 1:  # on top row
-        adjacent.append(idx + num_cols)  # adds below
-        if left:
-            adjacent += [
-                idx + num_cols + 1,
-                idx + num_cols + num_cols - 1,
-            ]  # adds [idx below to right one, on right boundary (same row below)]
-        elif right:
-            adjacent += [
-                idx + num_cols - 1,
-                idx + 1,
-            ]  # adds [idx below to left one, on left boundary (same row below)]
-        else:
-            adjacent += [
-                idx + num_cols - 1,
-                idx + num_cols + 1,
-            ]  # adds [below left, below right]
-        assert len(adjacent) == 5
-    if tot_boxes - num_cols + 1 <= idx <= tot_boxes:  # on bottom row
-        adjacent.append(idx - num_cols)  # adds above
-        if left:
-            adjacent += [
-                idx - num_cols + 1,
-                idx - 1,
-            ]  # adds [idx above to right one, on right boundary (same row above)]
-        if right:
-            adjacent += [
-                idx - num_cols - 1,
-                idx - num_cols - num_cols + 1,
-            ]  # adds [idx above to left one, on left boundary (same row above)]
-        else:
-            adjacent += [
-                idx - num_cols - 1,
-                idx - num_cols + 1,
-            ]  # adds [above left, above right]
-        assert len(adjacent) == 5
-    if num_cols <= idx <= tot_boxes - num_cols:  # not on boundary
-        if left:
-            adjacent += [
-                idx - 1,
-                idx - num_cols,
-                idx - num_cols + 1,
-                idx + num_cols + 1,
-                idx + num_cols + num_cols - 1,
-                idx + num_cols + 1,
-            ]  # add [above right (same row above), above, above right, below right (same row below), below, below right]
-        elif right:
-            adjacent += [
-                idx - num_cols - 1,
-                idx - num_cols,
-                idx - num_cols - num_cols + 1,
-                idx + num_cols - 1,
-                idx + num_cols,
-                idx + 1,
-            ]  # add [above left, above, above left (same row above), below left, below, below left (same row below)]
-        else:
-            adjacent += [
-                idx - num_cols - 1,
-                idx - num_cols,
-                idx - num_cols + 1,
-                idx + num_cols - 1,
-                idx + num_cols,
-                idx + num_cols + 1,
-            ]  # add [above left, above, above right, below left, below, below right]
-        assert len(adjacent) == 8
-    adjacent.sort()
-    return adjacent
+    x, y = xy_from_idx(idx, ncols)
+
+    xy = [
+        (x % ncols, y % nrows)  # wrap around
+        for x, y in [
+            (x - 1, y - 1),  # above left
+            (x, y - 1),  # above
+            (x + 1, y - 1),  # above right
+            (x - 1, y),  # left
+            (x + 1, y),  # right
+            (x - 1, y + 1),  # below left
+            (x, y + 1),  # below
+            (x + 1, y + 1),  # below right
+        ]
+    ]
+    return [idx_from_xy(x, y, ncols) for x, y in xy]
+
+
+def xy_from_idx(idx: int, ncols: int) -> tuple[int, int]:
+    """Find (x, y) tuple index from integer index, given number of columns"""
+    x = idx % ncols
+    y = idx // ncols
+    return x, y
+
+
+def idx_from_xy(x: int, y: int, ncols: int) -> int:
+    """Find integer index from (x, y) tuple index, given number of columns"""
+    return int(y * ncols + x)
