95c5695bf52e97c7ded8bad2dbf0b432a2e79f07
Fred Thomas <fred.thomas@ouce.ox.ac.uk>
2022-11-11 10:11:03 +0000

    update test to new bbox procedure

--- tests/integration/test_create_bbox_extracts.py
+++ tests/integration/test_create_bbox_extracts.py
@@ -5,6 +5,9 @@ def test_create_bbox_extracts():
     runner.run_snakemake_test(
         "create_bbox_extracts",
         (
-            "results/json/djibouti-latest_extracts.geojson",
+            "results/json/djibouti-latest_extracts/slice-0.geojson",
+            "results/json/djibouti-latest_extracts/slice-1.geojson",
+            "results/json/djibouti-latest_extracts/slice-2.geojson",
+            "results/json/djibouti-latest_extracts/slice-3.geojson",
         )
     )
