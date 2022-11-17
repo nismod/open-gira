d70201bd0a5eebf2d43be8b75832bb4760a24ec3
Fred Thomas <fred.thomas@ouce.ox.ac.uk>
2022-11-17 14:34:38 +0000

    update test naming

--- tests/integration/test_osm_to_geoparquet.py
+++ tests/integration/test_osm_to_geoparquet.py
@@ -1,9 +1,9 @@
 from . import runner
 
 
-def test_convert_to_geoparquet():
+def test_osm_to_geoparquet():
     runner.run_snakemake_test(
-        "convert_to_geoparquet",
+        "osm_to_geoparquet",
         (
             "results/geoparquet/djibouti-latest_filter-road/raw/slice-0_edges.geoparquet",
             "results/geoparquet/djibouti-latest_filter-road/raw/slice-0_nodes.geoparquet",
