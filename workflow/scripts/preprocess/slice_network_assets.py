726be5ff6f6d5ea2fb503cbbabbcbee4881d5769
Fred Thomas <fred.thomas@ouce.ox.ac.uk>
2023-04-05 16:49:08 +0100

    do not clip by shore buffer

--- workflow/scripts/preprocess/slice_network_assets.py
+++ workflow/scripts/preprocess/slice_network_assets.py
@@ -31,7 +31,6 @@ if __name__ == "__main__":
     targets_path: str = snakemake.input.targets
     powerplants_path: str = snakemake.input.powerplants
     admin_bounds_path: str = snakemake.input.admin_bounds
-    shore_buffer: float = snakemake.config["max_asset_shore_distance_deg"]
     gridfinder_out_path: str = snakemake.output.gridfinder
     targets_out_path: str = snakemake.output.targets
     powerplants_out_path: str = snakemake.output.powerplants
@@ -39,30 +38,10 @@ if __name__ == "__main__":
 
     logging.basicConfig(format="%(asctime)s %(process)d %(filename)s %(message)s", level=logging.INFO)
 
-    logging.info(f"Creating inland mask {shore_buffer} degrees from shore to select assets with")
-
     # read admin bounds for country in question
     countries = gpd.read_parquet(admin_bounds_path).rename(columns={"GID_0": "iso_a3"})
     country = countries[countries.iso_a3 == country_iso_a3]
     country = country[["iso_a3", "geometry"]].copy()
-
-    # create an approximate sea layer
-    low_res_countries = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
-    land: shapely.MultiPolygon = low_res_countries.geometry.unary_union
-    globe: shapely.Polygon = shapely.geometry.box(*land.bounds)
-    sea: shapely.MultiPolygon = shapely.difference(globe, land)
-
-    # if a large country, use a coarser boundary to accelerate subsetting
-    area_threshold_sq_deg = 100
-    country_geom, = country.geometry.values
-    if country_geom.area > area_threshold_sq_deg:
-        logging.info(f"Large geometry ({country_geom.area:.2f} sq. deg), using simplified country polygon")
-        low_res_country = low_res_countries[low_res_countries.iso_a3 == country_iso_a3]
-        low_res_country = low_res_country[["iso_a3", "geometry"]].copy()
-        country = low_res_country
-
-    # set country geometry to be some inland buffer from the shore
-    country.geometry: gpd.GeoSeries = shapely.intersection(sea.buffer(shore_buffer), country.geometry)
 
     os.makedirs(os.path.dirname(gridfinder_out_path), exist_ok=True)
 
