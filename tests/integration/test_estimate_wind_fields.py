8171fa1999d48816dac567357c9f65ea6220b1c4
Fred Thomas <fred.thomas@ouce.ox.ac.uk>
2023-10-16 16:50:09 +0100

    update test data paths

--- tests/integration/test_estimate_wind_fields.py
+++ tests/integration/test_estimate_wind_fields.py
@@ -5,6 +5,6 @@ def test_estimate_wind_fields():
     runner.run_snakemake_test(
         "estimate_wind_fields",
         (
-            "results/power/by_country/PRI/storms/IBTrACS/max_wind_field.nc",
+            "results/power/by_country/PRI/storms/IBTrACS/0/max_wind_field.nc",
         )
     )
