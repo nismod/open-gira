28b417190ba013f7d7beef7bc0b19454ac3a3607
Fred Thomas <fred.thomas@ouce.ox.ac.uk>
2023-12-11 17:00:04 +0000

    Update files for expected annual exposure by region test

--- tests/integration/test_exposure_by_admin_region.py
+++ tests/integration/test_exposure_by_admin_region.py
@@ -5,6 +5,6 @@ def test_exposure_by_admin_region():
     runner.run_snakemake_test(
         "exposure_by_admin_region",
         (
-            "results/power/by_country/PRI/exposure/IBTrACS/admin-level-1.geoparquet",
+            "results/power/by_country/PRI/exposure/IBTrACS/EAE_admin-level-1.gpq",
         )
     )
