634e8507999907b700ef0c5bb9cf402dea33bb15
Fred Thomas <fred.thomas@eci.ox.ac.uk>
2025-11-25 14:58:47 +0000

    Allow legacy or current imagemagick CLI

--- tests/visual_compare.sh
+++ tests/visual_compare.sh
@@ -1,29 +1,38 @@
-#! /bin/bash
+#!/bin/bash
 
-# given two image files as input, compare them with imagemagick's identfy
-# if the 'visual hashes' are equal, exit successfully, otherwise exit 1
+# Given two image files as input, compare them with imagemagick's identfy
+# If the 'visual hashes' are equal, exit successfully, otherwise exit 1
 
 set -e
 
-if [ -z $(which identify) ]; then
-    echo "require executable 'identify', please install imagemagick package"
+# Imagemagick v6 offers the 'identify' command
+# Imagemagick v7+ changes to the 'magick identify' command
+
+if [ -n "$(which magick)" ]; then
+    CMD="magick identify"
+else
+    if [ -n "$(which identify)" ]; then
+        CMD="identify"
+    else
+        echo "Cannot find 'magick' or 'identify' executable, please install imagemagick"
+        exit 1
+    fi
+fi
+
+if [ -z "$1" ]; then
+    echo "Require path to image file as argument 1"
     exit 1
 fi
 
-if [ -z $1 ]; then
-    echo "require path to image file as argument 1"
+if [ -z "$2" ]; then
+    echo "Require path to image file as argument 2"
     exit 1
 fi
 
-if [ -z $2 ]; then
-    echo "require path to image file as argument 2"
-    exit 1
-fi
-
-# see https://imagemagick.org/script/identify.php for more information
-if [[ $(identify -quiet -format "%#" $1) != $(identify -quiet -format "%#" $2) ]]; then
-    echo "files $1 and $2 are not a match"
-    echo "this could be because they are different formats, or a (subtle) visual difference"
-    echo "it should not be because of (embedded) file creation times"
+# See https://imagemagick.org/script/identify.php for more information
+if [[ $($CMD -quiet -format "%#" "$1") != $($CMD -quiet -format "%#" "$2") ]]; then
+    echo "Files $1 and $2 are not a match"
+    echo "This could be because they are different formats, or a (subtle) visual difference"
+    echo "It should not be because of (embedded) file creation times"
     exit 1
 fi
